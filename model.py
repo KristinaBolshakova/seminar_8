8e41b315cb47f1a6d98811e01a79b9e211fa7308
Alevtin <alevtin_antonov@mail.ru>
2022-11-18 21:32:01 +0300

    Added model.py

---
+++ model.py
@@ -0,0 +1,8 @@
+table = [
+    ['id', 'last_name', 'first_name', 'class_room', 'birth_date', 'address'],
+    ['1', 'Петров', 'Петр', '1', '01/01/2015', 'Городб улица дом']
+    ]
+
+#
+print(table[0][1])
+
