8fe4596827ee4f051431a781acaf44cd14bb7226
Kristina <Bolshakovakristy@gmail.com>
2022-11-20 15:38:45 +0300

    корректировка

--- controller.py
+++ controller.py
@@ -7,12 +7,10 @@ def run():
         view.input_data()
         num = int(input('Выберете действие: '))
         if num == 1:
-            model.show_dict()
+            model.show_dict_txt()
         elif num == 2:
-            model.enter_cont()
+            model.enter_stud_data()
         elif num == 3:
             pass
         elif num == 4:
-            pass
-        elif num == 5:
             pass
