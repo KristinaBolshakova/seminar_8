641e9b419bbe1ce8243f5f53d24dbbe62414ca1b
Alevtin_linux_home <antonov3017@gmail.com>
2022-11-21 21:29:28 +0300

    Добавил logger.py variables.py изменил view.py controller.py Работают 1,2,3 и 6 пункты меню. Надо посмотреть как id считать с файла и увеличивать с учетом ранее записанных id. Вывод на печать в терминале в табличной форме. Плюс п. 4 и 5 меню.

---
+++ controller.py
@@ -0,0 +1,20 @@
+import view
+import model
+
+def run():
+    while True:
+        view.input_data()
+        num = input('Выберите действие: ')
+        match num:
+            case '1':
+                model.show_dict_txt()
+            case '2':
+                model.enter_stud_data()
+            case '3':
+                model.save_data()
+            case '4':
+                pass
+            case '5':
+                pass
+            case '6':
+                exit()
